c2d02382a31dedccb900b22414a17e2a2feaa9ce
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2025-05-23 16:48:57 +0300

    Added Factory method

---
+++ FacrotyMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+delegate string Product();
+
+class Program
+{
+    static Product CreateProduct1() => () => "{Result of ConcreteProduct1}";
+    static Product CreateProduct2() => () => "{Result of ConcreteProduct2}";
+
+    static string SomeOperation(Func<Product> factoryMethod)
+    {
+        var product = factoryMethod();
+        return "Creator: The same creator's code has just worked with " + product();
+    }
+
+    static void ClientCode(Func<Product> factoryMethod)
+    {
+        Console.WriteLine("Client: I'm not aware of the creator's class, but it still works.\n" + SomeOperation(factoryMethod));
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("App: Launched with the ConcreteCreator1.");
+        ClientCode(CreateProduct1);
+
+        Console.WriteLine();
+
+        Console.WriteLine("App: Launched with the ConcreteCreator2.");
+        ClientCode(CreateProduct2);
+    }
+}
