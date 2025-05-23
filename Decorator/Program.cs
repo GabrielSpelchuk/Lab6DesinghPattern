b96e7f15ee7fb9691501c1600197f585b0c66849
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2025-05-23 17:18:42 +0300

    Added Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+delegate string Component();
+
+class Program
+{
+    static string ConcreteComponent() => "ConcreteComponent";
+
+    static Component DecoratorA(Component component) { return () => $"ConcreteDecoratorA({component})"; }
+    static Component DecoratorB(Component component) { return () => $"ConcreteDecoratorB({component})"; }
+
+    static void ClientCode(Component component) { Console.WriteLine("RESULT: " + component()); }
+
+    static void Main()
+    {
+        Console.WriteLine("Client: I get a simple component: ");
+        ClientCode(ConcreteComponent);
+        Console.WriteLine();
+
+        var decorated = DecoratorB(DecoratorA(ConcreteComponent));
+        Console.WriteLine("Client: Now I`ve got a decorated component: ");
+        ClientCode(decorated);
+    }
+
+}
--- Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d02382a31dedccb900b22414a17e2a2feaa9ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
